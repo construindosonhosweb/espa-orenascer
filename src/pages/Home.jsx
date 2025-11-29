11a9f2562e917af747dabf2536b081a17755349f
construindosonhosweb <construindosonhosweb436@gmail.com>
2025-11-29 00:02:46 -0300

    Refactor Home component to include multiple sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,19 @@
-export default function Home(){ return "Home"; }
+import Navbar from "../components/UI/Navbar";
+import Hero from "../components/Hero";
+import Collections from "../components/Collections";
+import Sustainability from "../components/Sustainability";
+import Essentials from "../components/Essentials";
+import About from "../components/About";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Collections />
+      <Sustainability />
+      <Essentials />
+      <About />
+    </>
+  );
+}
